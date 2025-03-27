595788e39438d326875b207c3b4e4f0ccb5d206b
sujan <suz_47@outlook.com>
2025-03-27 15:55:19 +0000

    Init App

--- PointerFixer-ProMaxUltra/main.m
+++ PointerFixer-ProMaxUltra/main.m
@@ -2,15 +2,20 @@
 //  main.m
 //  PointerFixer-ProMaxUltra
 //
-//  Created by Sujan Thapa on 27/03/2025.
+//  Created by Suz on 27/03/2025.
 //
 
 #import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
+#import <unistd.h> // for usleep()
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"PointerFixer-ProMaxUltra running...");
+        [[NSCursor crosshairCursor] set];
+        usleep(100000); // wait for 100 ms
+        [[NSCursor arrowCursor] set];
+        NSLog(@"Mouse pointer has been reset. Check again if pointer is fix or not. If not write a complain letter to APPLE.");
     }
     return 0;
 }
