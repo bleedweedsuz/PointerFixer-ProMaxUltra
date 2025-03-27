7d3e62817e41e86a1fac1ab773d6cdd80d38e0df
sujan <suz_47@outlook.com>
2025-03-27 15:12:31 +0000

    Initial Commit

---
+++ PointerFixer-ProMaxUltra/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PointerFixer-ProMaxUltra
+//
+//  Created by Sujan Thapa on 27/03/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
